b0f4d44809b7d0312497dede03bea16ea9640e16
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-10-15 11:43:15 +0200

    combinded template and view, next step is to add data to make data driven template

--- demo01/demos/02-demo-app/src/github.com/lss/webapp/main.go
+++ demo01/demos/02-demo-app/src/github.com/lss/webapp/main.go
@@ -2,35 +2,63 @@ package main
 
 import (
 	"html/template"
-	"log"
-	"net/http"
-)
+	"io/ioutil"
 
+	"net/http"
+	"os"
+)
 
 func main() {
 	templates := populateTemplates()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		requestedFile := r.URL.Path[1:]
-		template := templates.Lookup(requestedFile+".html")
+		//template := templates.Lookup(requestedFile + ".html")
+		template := templates[requestedFile + ".html"]
 		if template != nil {
-		   template.Execute(w, nil)
+			template.Execute(w, nil)
 		} else {
 			w.WriteHeader(http.StatusNotFound)
 		}
 	})
 
-	http.Handle("/img/",http.FileServer(http.Dir("/Users/foureverhh/go/src/1-creating-web-applications-go-update-m1-exercise-files/demos/02-demo-app/public")))
-	http.Handle("/css/",http.FileServer(http.Dir("/Users/foureverhh/go/src/1-creating-web-applications-go-update-m1-exercise-files/demos/02-demo-app/public")))
-	http.ListenAndServe(":8000",nil)
+	http.Handle("/img/", http.FileServer(http.Dir("/Users/foureverhh/go/src/pluralSightWebAppWithGo/demo01/demos/02-demo-app/public")))
+	http.Handle("/css/", http.FileServer(http.Dir("/Users/foureverhh/go/src/pluralSightWebAppWithGo/demo01/demos/02-demo-app/public")))
+	http.ListenAndServe(":8000", nil)
 }
-
-func populateTemplates() *template.Template {
-	result:= template.New("template")
-	const basePath = "templates"
-	template.Must(result.ParseGlob(basePath + "/*.html"))
+func populateTemplates() map[string]*template.Template {
+	result := make(map[string]*template.Template)
+	const basePath = "/Users/foureverhh/go/src/pluralSightWebAppWithGo/demo01/demos/02-demo-app/templates"
+	layout := template.Must(template.ParseFiles(basePath + "/_layout.html"))
+	template.Must(layout.ParseFiles(basePath+"/_header.html", basePath+"/_footer.html"))
+	dir, err := os.Open(basePath + "/content")
+	if err != nil {
+		panic("Failed to open template blocks directory: " + err.Error())
+	}
+	files, err := dir.Readdir(-1)
+	if err != nil {
+		panic("Failed to read contents of content directory")
+	}
+	for _, file := range files {
+		f, err := os.Open(basePath + "/content/" + file.Name())
+		if err != nil {
+			panic("Failed to open template '" + file.Name() + "'")
+		}
+		content, err := ioutil.ReadAll(f)
+		if err != nil {
+			panic("Failed to read content from file '"+file.Name()+"'")
+		}
+		f.Close()
+		teml := template.Must(layout.Clone())
+		_, err = teml.Parse(string(content))
+		if err != nil {
+			panic("Failed to parse contents of '"+file.Name()+"'")
+		}
+		result[file.Name()] =teml
+	}
 	return result
 }
-/* 
+
+/*
 //This does not work with template - content
 func populateTemplates() *template.Template {
 	result := template.New("templates")
@@ -38,7 +66,7 @@ func populateTemplates() *template.Template {
 	template.Must(result.ParseGlob(basePath + "/*.html"))
 	return result
 }
- */
+*/
 /*
 func main() {
 	http.ListenAndServe(":8000", http.FileServer(http.Dir("/Users/foureverhh/go/src/1-creating-web-applications-go-update-m1-exercise-files/demos/02-demo-app/public")))
---
+++ demo01/demos/02-demo-app/src/github.com/lss/webapp/viewmodel/base.go
@@ -0,0 +1,13 @@
+package viewmodel
+
+//Base struct
+type Base struct{
+	Title string
+}
+
+//NewBase definition
+func NewBase() Base{
+	return Base{
+		Title: "Lemonad Stand Supply",
+	}
+}
