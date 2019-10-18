530a0fff649ea7910eae0f3d03db32535ea15d6a
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2019-10-18 11:55:31 +0200

    FuncMap and customised func

--- pipeline/main.go
+++ pipeline/main.go
@@ -12,18 +12,20 @@ type Product struct {
 	Name  string
 	Price float32
 }
-
+/* 
 //PriceWithTax method
 func (p Product) PriceWithTax() float32 {
 	return p.Price * (1 + tax)
 }
+*/
 
 const templateString = `
 {{- "Item Information" }}
 Name: {{ .Name}}
 Price: {{ printf "$%.2f" .Price}}
-Price with Tax: {{ .PriceWithTax | printf "$%.2f"}}
+Price with Tax: {{ calctax .Price | printf "$%.2f"}}
 `
+//Price with Tax: {{ .PriceWithTax | printf "$%.2f"}}
 
 func main() {
 
@@ -32,6 +34,15 @@ func main() {
 		Price: 2.16,
 	}
 
-	t:=template.Must(template.New("").Parse(templateString))
+	fm := template.FuncMap{}
+
+	fm["calctax"] = func (price float32) float32 {
+		return price * (1 + tax)
+	}
+
+	t := template.Must(template.New("").Funcs(fm).Parse(templateString))
+
+	//This is for PriceWithTax
+	//t:=template.Must(template.New("").Parse(templateString))
 	t.Execute(os.Stdout, p)
 }
